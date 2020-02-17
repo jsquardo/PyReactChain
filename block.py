6dc99f57e64fcf622e8e5b75be8178e00851dd58
Johnny Squardo <johnnysquardo@gmail.com>
2020-02-17 17:29:18 -0500

    Moving  files

---
+++ block.py
@@ -0,0 +1,20 @@
+class Block:
+    """
+    Block: a unit of storage.
+    """
+
+    def __init__(self, data):
+        self.data = data
+
+    def __repr__(self):
+        return f'Block - data: {self.data}'
+
+
+def main():
+    block = Block('foo')
+    print(block)
+    print(f'block.py __name__: {__name__}')
+
+
+if __name__ == '__main__':
+    main()
